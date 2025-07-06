482261c225a2f0f5d5376867bec05589a353d72b
CAHC2509 <cahc2509.code@gmail.com>
2025-07-06 08:36:34 -0500

    Scripting cleared
    - All scripts and functionalities were removed to begin with a new architecture

--- Assets/Scripts/SO's/DescriptionMessage.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "Scriptable Objects/Settings/Description Message", fileName = "Description Message")]
-public class DescriptionMessage : ScriptableObject
-{
-    [field: TextArea]
-    [field: SerializeField] public string Message { get; private set; }
-}
