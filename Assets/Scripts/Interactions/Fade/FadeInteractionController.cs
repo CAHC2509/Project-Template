791725835ea3575752a608f515c698c5b6aa4c9c
CAHC2509 <cahc2509.code@gmail.com>
2025-08-10 21:47:35 -0500

    Container added to fade interaction controller
    - By adding the fade enabling and disabling using a container instead of a single image, the possible problems due multiple active canvases will be reduced.

--- Assets/Scripts/Interactions/Fade/FadeInteractionController.cs
+++ Assets/Scripts/Interactions/Fade/FadeInteractionController.cs
@@ -4,19 +4,20 @@ using DG.Tweening;
 
 public class FadeInteractionController : InteractionBaseController
 {
+    [SerializeField] private GameObject fadeCanvas;
     [SerializeField] private Image fadeImage;
     [SerializeField] private float fadeDuration = 1f;
 
     public float FadeDuration => fadeDuration;
 
-    public override void Initialize() => fadeImage.gameObject.SetActive(false);
+    public override void Initialize() => fadeCanvas.SetActive(false);
 
     public override void BeginInteraction()
     {
         Color color = fadeImage.color;
         color.a = 0f;
         fadeImage.color = color;
-        fadeImage.gameObject.SetActive(true);
+        fadeCanvas.SetActive(true);
 
         fadeImage.DOFade(1f, fadeDuration);
     }
@@ -30,5 +31,5 @@ public class FadeInteractionController : InteractionBaseController
         fadeImage.DOFade(0f, fadeDuration).OnComplete(Conclude);
     }
 
-    public override void Conclude() => fadeImage.gameObject.SetActive(false);
+    public override void Conclude() => fadeCanvas.SetActive(false);
 }
